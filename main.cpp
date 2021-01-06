387e300845a4f55e3d2e16fb1bc30ae4b418988e
jatozy <infrastructure@jato-engineering.de>
2021-01-06 15:58:23 +0100

    Add clang-format and clang-tidy

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+int main(int, char **)
+{
+    constexpr bool condition = true;
+
+    if (condition)
+    {
+        std::cout << "Hello, true world!\n";
+    }
+    else
+    {
+        std::cout << "Hello, wrong world!\n";
+    }
 }
